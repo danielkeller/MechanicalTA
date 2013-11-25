f3f67a51ea9304c3b851bd8c78b11af2d9f876a9
Dan Keller <dbk40@case.edu>
2013-11-25 01:11:12 -0500

    validates constructor

--- api/mta/api/AssignmentRunner.java
+++ api/mta/api/AssignmentRunner.java
@@ -1,5 +1,6 @@
 package mta.api;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class AssignmentRunner extends BlockJUnit4ClassRunner {
 		testClasses = testSet;
 	}
 	
+	//called when we're runnning in normal junit mode
 	public AssignmentRunner(Class<?> klass)
 			throws InitializationError {
 		super(klass);
@@ -24,7 +26,14 @@ public class AssignmentRunner extends BlockJUnit4ClassRunner {
 	
 	@Override
 	protected Object createTest() throws Exception {
-		Class<?> iface = getTestClass().getOnlyConstructor().getParameterTypes()[0];
+		
+		//running in junit mode
+		if (testClasses == null) {
+			throw new Exception("Could not find 0-argument constructor");
+		}
+			
+		Constructor<?> ifaceCtor = getTestClass().getOnlyConstructor();
+		Class<?> iface = ifaceCtor.getParameterTypes()[0];
 		Class<?> impl = null;
 		
 		for (Class<?> cls : testClasses)
@@ -57,24 +66,41 @@ public class AssignmentRunner extends BlockJUnit4ClassRunner {
         validateNoTestAnnotations(errors);
 	}
 	
+	//has to be in separate methods so we can initialize before calling it
 	@Override
 	protected void validateConstructor(List<Throwable> errors) {
-        validateOnlyOneConstructor(errors);
+		validateOnlyOneConstructor(errors);
         validateOneArgConstructor(errors);
 	}
-	
 	protected void validateOneArgConstructor(List<Throwable> errors) {
-        Class<?>[] params = getTestClass().getOnlyConstructor().getParameterTypes();
+        Constructor<?>[] ctors = getTestClass().getJavaClass().getConstructors();
         
-        if (params.length != 1) {
-	        String gripe = "Test class should have exactly one public one-argument constructor";
+        Constructor<?> onearg = null;
+        for (Constructor<?> ctor : ctors) {
+        	if (ctor.getParameterTypes().length == 1) {
+        		if (onearg == null) {
+        			onearg = ctor;
+        		}
+        		else {
+        	        String gripe = "Test class should have exactly one " +
+        	        		"public one-argument constructor";
+        	        errors.add(new Exception(gripe));
+        	        return;
+                }
+        	}
+        }
+        
+        if (onearg == null) {
+	        String gripe = "Test class should have exactly one " +
+	        		"public one-argument constructor";
 	        errors.add(new Exception(gripe));
 	        return;
         }
-        
-        if (!params[0].isInterface()) {
+
+        if (!onearg.getParameterTypes()[0].isInterface()) {
 	        String gripe = "Test class should have exactly one public constructor" +
 	        		" taking one interface type";
+	        System.out.println(onearg.getParameterTypes()[0].getName());
 	        errors.add(new Exception(gripe));
         }
 	}
