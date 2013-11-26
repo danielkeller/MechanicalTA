a570295eb20a5333b50b313d7cad1c7c5e301c14
Dan Keller <dbk40@case.edu>
2013-11-26 12:45:56 -0500

    made future work right

--- src/mta/util/Future.java
+++ src/mta/util/Future.java
@@ -5,39 +5,26 @@ import com.trolltech.qt.core.*;
 
 public abstract class Future<T> extends QObject {
 	public Future(Object recipient, String slot) {
-		done.connect(recipient, slot, Qt.ConnectionType.QueuedConnection);
+		done.connect(recipient, slot);
 	}
 
 	public synchronized T get() {return val;}
 	protected abstract T evaluate();
 	
-	public void start() {
-		if (running)
-			return;
-		running = true;
-		
+	public synchronized void start() {		
 		final QThread th = new QThread(new Runnable () {
 			@Override
 			public void run() {
 				synchronized (this_) {
-					try {
-						T tmp = evaluate();
-						val = tmp;
-						done.emit();
-					} finally {
-						running = false;
-						this_.moveToThread(orig);
-					}
+					val = evaluate();
 				}
 			}
 		});
-		moveToThread(th);
+		th.finished.connect(done, Qt.ConnectionType.QueuedConnection);
 		th.start();
 	}
 	
 	private Signal0 done = new Signal0();
 	private T val = null;
-	private Thread orig = thread();
 	private Future<T> this_ = this;
-	private boolean running = false;
 }
