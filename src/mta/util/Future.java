17d512a4e904da39f482bf484a9fcb6269a25338
Dan Keller <dbk40@case.edu>
2013-11-26 13:26:48 -0500

    improve future performance

--- src/mta/util/Future.java
+++ src/mta/util/Future.java
@@ -11,20 +11,37 @@ public abstract class Future<T> extends QObject {
 	public synchronized T get() {return val;}
 	protected abstract T evaluate();
 	
-	public synchronized void start() {		
-		final QThread th = new QThread(new Runnable () {
-			@Override
-			public void run() {
-				synchronized (this_) {
-					val = evaluate();
-				}
-			}
-		});
+	public synchronized void start() {
+		//ensure that the last thread to start gets to set the value
+		//as opposed to the last thread to finish
+		if (running != null) {
+			running.canFinish = false;
+			//don't send extra signals
+			th.finished.disconnect();
+		}
+		
+		running = new FRunnable();
+		th = new QThread(running);
+		
 		th.finished.connect(done, Qt.ConnectionType.QueuedConnection);
 		th.start();
+	}
+	
+	class FRunnable implements Runnable {
+		boolean canFinish = true;
+		@Override
+		public void run() {
+			T temp = evaluate();
+			synchronized (this_) {
+				if (canFinish)
+					val = temp;
+			}
+		}
 	}
 	
 	private Signal0 done = new Signal0();
 	private T val = null;
 	private Future<T> this_ = this;
+	FRunnable running = null;
+	QThread th = null;
 }
